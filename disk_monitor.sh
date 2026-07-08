9650477ad619c97cf8b22cc6fb964febaa39eff0
Ivan <ivandarii2024@gmail.com>
2026-07-07 22:38:19 -0400

    feat: add initiall disk monitoring script

---
+++ disk_monitor.sh
@@ -0,0 +1,8 @@
+THRESHOLD=80
+CURRENT_USAGE=$(df / | awk 'NR==2 {print $5}' | tr -d '%')
+echo "Current disk usage: $CURRENT_USAGE%"
+if [ "CURRENT_USAGE" -gt "THRESHOLD" ]; then
+echo "! ALERT! Disk usage is about $THRESHOLD%! Free up disk immediately!"
+else
+echo " Everything is ok. Disk space is sufficient."
+fi
